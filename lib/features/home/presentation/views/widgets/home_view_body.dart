52e604c02f652c3be84388ce678fa1d566c3d368
Essam al-rawi <essamhamdy198@gmail.com>
2025-12-23 19:52:00 +0200

    feat: add RecommendationDoctor widget and integrate it into HomeViewBody

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:docdoc/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:docdoc/features/home/presentation/views/widgets/doctor_speciality_section.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_styles.dart';
 import 'find_nearby_section.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -18,8 +19,53 @@ class HomeViewBody extends StatelessWidget {
           FindNearbySection(),
           SizedBox(height: 24),
           DoctorSpecialitySection(),
+          SizedBox(height: 22),
+          RecommendationDoctor(),
         ],
       ),
     );
+  }
+}
+
+class RecommendationDoctor extends StatelessWidget {
+  const RecommendationDoctor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Recommendation Doctor',
+              style: AppStyles.styleRegular18px(context).copyWith(
+                color: const Color(0xFF242424),
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                height: 1.60,
+              ),
+            ),
+            Text(
+              'See All',
+              textAlign: TextAlign.right,
+              style: AppStyles.styleRegular12px(
+                context,
+              ).copyWith(color: const Color(0xFF247CFF), height: 1.50),
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+}
+
+class RecommendationDoctorCard extends StatelessWidget {
+  const RecommendationDoctorCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [Container()]);
   }
 }
